ebd2e919814e8ed8f99d18eb0ce9f7226ef4c207
root <root@dk00.iotech.co.th>
2018-01-24 20:31:18 +0700

    Add support thai font

--- routes/api.php
+++ routes/api.php
@@ -27,15 +27,16 @@ $qrCode
     ->setText($request->text)
     ->setSize(800)
     ->setPadding(15)
+    ->setLabelFontPath('/var/www/html/public/assets/Kanit/Kanit-Medium.ttf')
     ->setErrorCorrection('high')
     ->setForegroundColor(array('r' => 0, 'g' => 0, 'b' => 0, 'a' => 0))
     ->setBackgroundColor(array('r' => 255, 'g' => 255, 'b' => 255, 'a' => 0))
-    ->setLabel('Scan: ' . $request->title)
-    ->setLabelFontSize(18)
+    ->setLabel('#สแกนเพื่อดู: ' . $request->title)
+    ->setLabelFontSize(22)
     ->setImageType(QrCode::IMAGE_TYPE_PNG)
 ;
-
-return response(base64_decode($qrCode->generate()))->header('Content-type','image/png')->header("Content-Disposition", "inline; filename=\"{$filename}\"");
-// echo '<img src="data:'.$qrCode->getContentType().';base64,'.$qrCode->generate().'" />';
-
+echo '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">';
+// return response(base64_decode($qrCode->generate()))->header('Content-type','image/png')->header("Content-Disposition", "inline; filename=\"{$filename}\"");
+echo '<div style="text-align: center;"><img src="data:'.$qrCode->getContentType().';base64,'.$qrCode->generate().'" />';
+// echo '<h1>Scan to ' . $request->title . '</h1></div>';
 });
